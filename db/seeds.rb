a5b83c6f1a0fdaf4f6bc8548e1954698d6554b87
Nikita Nagras <snbnikita@gmail.com>
2024-02-13 17:02:15 +0900

    skdfjs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+require "faker"
+
+10.times do
+  Article.create(title: Faker::Game.title, content: Faker::Quote.famous_last_words)
+end
+
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
